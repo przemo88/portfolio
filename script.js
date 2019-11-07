6d66cbc793a26a0f7597c3cbac8533d957a4bc9f
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-11-07 13:33:24 +0100

    fix code

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ $(document).ready(function () {
         $("li.active").removeClass("active");
         $(this).addClass("active");
     });
+
+    $('a[href^="#"]').on('click', function (event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
+    });
+
 });
+
+
 
