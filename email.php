906bdb34406f3a37bb0e9533532bf5d3f0e06506
Przemyslaw Przewoznik <blagoff88@gmail.com>
2020-04-30 02:12:32 +0200

    fix rwd & menu add email

---
+++ email.php
@@ -0,0 +1,28 @@
+<?php
+
+    $name = htmlspecialchars(stripslashes(trim($_POST['name'])));
+
+    $email = filter_var(htmlspecialchars(stripslashes(trim($_POST["email"])), FILTER_SANITIZE_EMAIL));
+
+    $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
+    
+    $my_email = "kontakt@przemyslawprzewoznik.pl";
+  
+    $subject = "Wiadomość od $name wysłana przez formularz kontaktowy.";
+    
+    $email_content = "Imię: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Wiadomość: $message\n";
+    
+    $email_headers = "Od: $email";
+    
+    $send_email = mail($my_email, $subject, $email_content, $email_headers);
+    
+    if($send_email){ 
+        echo "Dziękuję,  twoja wiadomość została wysłana"; 
+    } else {
+  
+        echo "Nie udało się wysłać twojej wiadomości. Spróbuj ponownie alb użyj linka w sekcji o mnie ";
+    }
+ 
+?>
--- script.js
+++ script.js
@@ -18,5 +18,39 @@ $(document).ready(function () {
 
 });
 
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var width = $(window).width();
+
+    if (width > 768) {
+        if (scroll < 1) {
+            $('ul li').first().attr('style', 'font-size: 15px');
+            $('ul li').not(':first-child').attr('style', 'font-size: 15px');
+        }
+        else if (scroll > 601 && scroll < 2800) {
+            $('ul li').eq(1).attr('style', 'font-size: 25px');
+            $('ul li').not(':eq(1)').attr('style', 'font-size: 10px');
+        }
+        else if (scroll > 2801 && scroll < 3400) {
+            $('ul li').eq(2).attr('style', 'font-size: 25px');
+            $('ul li').not(':eq(2)').attr('style', 'font-size: 10px');
+        }
+        else if (scroll > 3401) {
+            $('ul li').eq(3).attr('style', 'font-size: 25px');
+            $('ul li').not(':eq(3)').attr('style', 'font-size: 10px');
+        }
+    }
+
+});
+
+$(document).ready(function () {
+    $("input, textarea")
+        .focusout(function () {
+            $("label").not().attr('style', 'font-size: 20px;');
+        })
+        .focusin(function () {
+            $("label").not().attr('style', 'font-size: 10px;');
+        });
+});
 
 
